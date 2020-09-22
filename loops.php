39049091a2da15594489e946f51d66fd11a3cb68
mz342@njit.edu <mz342@njit.edu>
2020-09-21 21:42:14 -0400

    PHP Loops Homework

---
+++ loops.php
@@ -0,0 +1,26 @@
+<?php
+// Your code here!
+$arr = ["1","2","3","4"];
+
+for ($i = 0; $i < count ($arr); $i++)
+{
+    echo "$arr[$i]\n";
+}
+for ($i = 0; $i < count ($arr); $i++)
+{
+    if ($arr[$i] % 2 == 0)
+    {
+        echo "$arr[$i]\n";
+
+    }
+
+
+
+
+
+
+
+
+}
+?>
+
