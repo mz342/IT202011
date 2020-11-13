78bf724255b3ebb2c5e390f3368316a3ab773c48
mz342 <mz342@njit.edu>
2020-11-13 00:45:14 +0000

    hw files

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -4,15 +4,21 @@
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <nav>
-<ul class="nav">
-    <li><a href="home.php">Home</a></li>
-    <?php if (!is_logged_in()): ?>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-    <?php endif; ?>
-    <?php if (is_logged_in()): ?>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    <?php endif; ?>
-</ul>
+    <ul class="nav">
+        <li><a href="home.php">Home</a></li>
+        <?php if (!is_logged_in()): ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (has_role("Admin")): ?>
+            <li><a href="test_create_products.php">Create product</a></li>
+            <li><a href="test_list_products.php">View product</a></li>
+            <li><a href="test_create_cart.php">Create cart</a></li>
+            <li><a href="test_list_cart.php">View cart</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()): ?>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
 </nav>
