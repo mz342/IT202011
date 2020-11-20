8759b01878e33a9acaddb1bd90bbc338d38fc669
mz342 <mz342@njit.edu>
2020-11-20 03:26:20 +0000

    feature 1 assignment fixes

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -11,10 +11,14 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="register.php">Register</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")): ?>
-            <li><a href="test_create_products.php">Create product</a></li>
-            <li><a href="test_list_products.php">View product</a></li>
-            <li><a href="test_create_cart.php">Create cart</a></li>
-            <li><a href="test_list_cart.php">View cart</a></li>
+            <li><a href="test_create_products.php">Create Products</a></li>
+            <li><a href="test_list_products_admin.php">View Products</a></li>
+            <li><a href="test_create_cart.php">Create Carts</a></li>
+            <li><a href="test_list_cart.php">View Carts</a></li>
+        <?php endif; ?>
+        <?php if (has_role("User")): ?>
+            <li><a href="test_list_products.php">View Products</a></li>
+            <li><a href="test_list_cart.php">View Carts</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
--- project/test_create_products.php
+++ project/test_create_products.php
@@ -1,4 +1,4 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+ <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
 if (!has_role("Admin")) {
     //this will redirect to login and kill the rest of this script (prevent it from executing)
@@ -16,6 +16,10 @@ if (!has_role("Admin")) {
 	<input type="number" min="0" name="quantity"/>
 	<label>Description</label>
 	<input type="text" name="description"/>
+	<label>Category</label>
+	<input type="text" name="category"/>
+	<label>Visibility</label>
+	<input type="number" min="0" name="visibility"/>
 	<input type="submit" name="save" value="Create"/>
 </form>
 
@@ -28,15 +32,19 @@ if(isset($_POST["save"])){
 	$quantity = $_POST["quantity"];
 	$desc = $_POST["description"];
 	//$nst = date('Y-m-d H:i:s');//calc
+	$category = $_POST["category"];
+	$visibility = $_POST["visibility"];
 	$user = get_user_id();
 	$db = getDB();
-	$stmt = $db->prepare("INSERT INTO Products (name, price, quantity, description, user_id) VALUES(:name, :pr, :quantity, :desc, :user)");
+	$stmt = $db->prepare("INSERT INTO Products (name, price, quantity, description, category, visibility, user_id) VALUES(:name, :pr, :quantity, :desc, :category, :visibility, :user)");
 	$r = $stmt->execute([
 		":name"=>$name,
 		":pr"=>$pr,
 		":quantity"=>$quantity,
 		":desc"=>$desc,
 		//":nst"=>$nst,
+		":category"=>$category,
+		":visibility"=>$visibility,
 		":user"=>$user
 	]);
 	if($r){
