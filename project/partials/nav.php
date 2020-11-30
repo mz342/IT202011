e526a6bc97566ade04cd8d5944ec4fe5be19ecaa
mz342 <mz342@njit.edu>
2020-11-30 04:32:38 +0000

    updated nav and added shop

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -19,6 +19,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php if (has_role("User")): ?>
             <li><a href="test_list_products.php">View Products</a></li>
             <li><a href="test_list_cart.php">View Carts</a></li>
+	    <li><a href="shop.php">Shop</a></li>
+	    <li><a href="MyCart.php">Cart</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
